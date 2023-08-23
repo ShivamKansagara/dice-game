dc4aaff2948d6c31ac5bb1c5db5dae7600296036
Shivam Kansagara <202001086@daiict.ac.in>
2023-08-23 20:20:31 +0530

    g

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ if (randomNumber > randomNumber1) {
   document.getElementsByTagName("h1")[0].textContent = "Player 1 wins";
 } else if (randomNumber < randomNumber1) {
   document.getElementsByTagName("h1")[0].textContent = "Player 2 wins";
-} else {
+} else if (randomNumber === randomNumber1) {
   document.getElementsByTagName("h1")[0].textContent = "OOPS there is a Tie";
 }
