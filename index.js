3d73cb3691fc68a6b90f34c950d509636337cf55
Shivam Kansagara <202001086@daiict.ac.in>
2023-08-23 20:16:31 +0530

    ...

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-let randomNumber = Math.random() * 6;
-randomNumber = Math.floor(randomNumber) + 1;
+let randomNumber = Math.floor(Math.random() * 6) + 1;
 let one = "./images/dice" + randomNumber + ".png";
 document.getElementById("one").setAttribute("src", one);
 
-let randomNumber1 = Math.random() * 6;
-randomNumber1 = Math.floor(randomNumber1) + 1;
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 let two = "./images/dice" + randomNumber1 + ".png";
 document.getElementById("two").setAttribute("src", two);
 
@@ -12,5 +10,6 @@ if (randomNumber > randomNumber1) {
   document.getElementsByTagName("h1")[0].textContent = "Player 1 wins";
 } else if (randomNumber < randomNumber1) {
   document.getElementsByTagName("h1")[0].textContent = "Player 2 wins";
-} else
+} else {
   document.getElementsByTagName("h1")[0].textContent = "OOPS there is a Tie";
+}
